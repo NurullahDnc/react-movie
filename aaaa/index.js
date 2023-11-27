b1aaa78987a43048975be80e424b6af7f681bff8
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2023-11-27 22:34:55 +0300

    proje baslatıldı

---
+++ aaaa/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Doğru şekilde react-dom'u içe aktarın
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
