39ca189ba98707c3575ce6c466598fb2d2975305
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2023-11-27 22:39:00 +0300

    copyalanmıs olan dosyalar silindi

--- aaaa/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; // Doğru şekilde react-dom'u içe aktarın
-import App from './components/App'
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
