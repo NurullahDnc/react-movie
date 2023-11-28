822cf1b346c81ecc6cca7cfa67cbdf72a632b03e
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2023-11-28 23:02:02 +0300

    input arama islemi yapıldı

--- src/components/App.js
+++ src/components/App.js
@@ -29,8 +29,32 @@ class App extends React.Component {
                 "rating": 7.9,
                 "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
                 "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
+            },
+            {
+                "id": 4,
+                "name": "Rogue",
+                "rating": 7.4,
+                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg"
+            },
+
+            {
+                "id": 5,
+                "name": "Project Power",
+                "rating": 6.7,
+                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg"
+            },
+
+            {
+                "id": 6,
+                "name": "Superman",
+                "rating": 7.6,
+                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg"
             }
-        ]
+        ],
+        searcQuery: ""  /*inputa girilen degeri buraya atıyoruz */
     }
  
     //delete islemi
@@ -38,36 +62,46 @@ class App extends React.Component {
         const newmovieList = this.state.movies.filter( /* flimleri filitreliyor sildikren sonra tekrar liste olsuturacak */
             m => m.id !== movie.id  /* movie id esit olmayanları getir */
         )
-        // this.setState({  /*2.yontem filimler ilimizde olmasa kulanırırz */
-        //     movies: newmovieList    /*olusturdugummuz newmovieList guncel halini movies'e atıyoruz */
+        // this.setState({  /*2.yontem filimler elimizde olmasa kulanırırz */
+        //     movies: newmovieList  
         // })
-    
+        //guncel degerleri atma
         this.setState(state =>({    /*parametre olarak var olan satate alıyoruz, var olan state gunceliyoruz  */
             movies: newmovieList    /*olusturdugummuz newmovieList guncel halini movies'e atıyoruz */ 
         }))
     }
 
+    //search islemi
+    searcMovie =(event)=>{
+        // console.log(event.target.value)
+        this.setState({searcQuery: event.target.value})
+    }
+
     render() {
+        //filitreleme islemi
+        let filterMovies = this.state.movies.filter(    
+            (movie)=>{  /* filimler movie parametresinde  */
+                return movie.name.toLocaleLowerCase().indexOf(this.state.searcQuery.toLocaleLowerCase()) !== -1     /*aranan deger filim'in iceriside var ise geriye, gon yoksa -1 bos don */ 
+            }
+        )
+
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <SearchBar />
+                        <SearchBar 
+                            propsSearchMovie ={this.searcMovie} /*searcMovie func. , propsSearchMovie degiskenin icerisine at ve props ile gonder gonder*/
+                        />
                     </div>
                 </div>
 
                 <MovieList
-                    movies={this.state.movies}   /* props olarak gonderme state icerisinde tanımlanan data'yı alıyor. */
-                    propsDeletMovie= {this.deletMovie}
+                    movies={filterMovies}   /* props olarak gonderme, filitrelenen ogeleri, ekranda gosterilecek kartlar*/
+                        propsDeletMovie= {this.deletMovie}
                     />
-                    
-
             </div>
         )
-
     }
-
-
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,33 +2,33 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-    state= {    /*setstate olsuturuldu */
-        searcQuery:""   
-    }
-
-    handleFormSubmit =(e)=>{
+    handleFormSubmit = (e) => {
         e.preventDefault()
-
     }
 
     render() {
 
-        return  (
+        return (
             <form onSubmit={this.handleFormSubmit}>
                 <div className='form-row mb-5 mt-4'>
                     <div className='col-12'>
-                        <input 
-                        onChange={(event)=> this.setState({searcQuery: event.target.value})}    // input valuesini  serstate ile state'ye ekliyor
-                        value={this.state.searcQuery}  /*inputtaki v alue state icerisine atldı, state'de ki deger input'un valuesine atıldı */
-                        type='text' className='form-control' placeholder='search a movies'></input>
+                        <input
+                            onChange={this.props.propsSearchMovie}    // propdan gelen degeri calıstır, event parametresi gonderiyor
+                            type='text'
+                            className='form-control'
+                            placeholder='search a movies'>
+
+                        </input>
                     </div>
                 </div>
             </form>
         )
 
-        
+
     }
 }
 
 
 export default SearchBar;
+
+//! onChange()  = her degisklikte calısıyor
