747e9ec6824e60e558d2682489ae052f00002ef0
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2023-11-29 16:35:36 +0300

    axios ile  get, delete  / fetch ile delete islemleri yapıldı

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import axios from 'axios';
 
 
 class App extends React.Component {
@@ -10,41 +11,77 @@ class App extends React.Component {
         searcQuery: ""  /*inputa girilen degeri buraya atıyoruz */
     }
 
-    //  olusturudugumuz .json dosyasına istek atma
-    async componentDidMount(){
-        const baseURL ="http://localhost:3002/movies"   // datayı olsuturdugumuz json dosyasından istek atarak aldık
-        const response = await fetch(baseURL);
-        console.log(response);
-        const data =await response.json();
-        this.setState({movies: data});  // data icerisindeki veri movies'e atıldı
+
+    // //* 1.yontem fect ile  olusturudugumuz .json dosyasına istek atma
+    // async componentDidMount() {
+    //     const baseURL = "http://localhost:3002/movies";   // datayı olsuturdugumuz json dosyasından, istek atarak aldındı - fake rest api
+    //     const response = await fetch(baseURL);
+    //     console.log(response);
+    //     const data = await response.json();
+    //     this.setState({ movies: data });  // data icerisindeki veri movies'e atıldı
+    // }
+
+
+    //  2. yontem axios ile istek atma
+    async componentDidMount() {
+        const response = await axios.get("http://localhost:3002/movies");  // istek atıyoruz 
+        // console.log(response)
+        this.setState({ movies: response.data });
     }
- 
-    //delete islemi
-    deletMovie = (movie) => {
-        const newmovieList = this.state.movies.filter( /* flimleri filitreliyor sildikren sonra tekrar liste olsuturacak */
-            m => m.id !== movie.id  /* movie id esit olmayanları getir */
+
+
+    // //* 1 api olmadan delete islemi
+    // deletMovie = (movie) => {
+    //     const newmovieList = this.state.movies.filter( /* flimleri filitreliyor sildikren sonra tekrar liste olsuturacak */
+    //         m => m.id !== movie.id  /* movie id esit olmayanları getir */
+    //     )
+    //     //guncel degerleri atma
+    //     this.setState(state => ({    /*parametre olarak var olan satate alıyoruz, var olan state gunceliyoruz  */
+    //         movies: newmovieList    /*olusturdugummuz newmovieList guncel halini movies'e atıyoruz */
+    //     }))
+    // }
+
+
+    // //* 2 fect api ile delete islemi
+    // deletMovie = async (movie) => {
+    //     const baseURL = `http://localhost:3002/movies/${movie.id}` // movie.id gore silme islemi yapıyor 
+    //     await fetch(baseURL, {
+    //         method: "DELETE" // apı'de delete islemi yapıyor
+    //     })
+
+    //     const newmovieList = this.state.movies.filter(
+    //         m => m.id !== movie.id
+    //     )
+    //     this.setState(state => ({
+    //         movies: newmovieList
+    //     }))
+    // }
+
+    //* 2 axios ile delete islemi
+    deletMovie = async (movie) => {
+        axios.delete(`http://localhost:3002/movies/${movie.id}`);   
+
+        const newmovieList = this.state.movies.filter( 
+            m => m.id !== movie.id  
         )
-        // this.setState({  /*2.yontem filimler elimizde olmasa kulanırırz */
-        //     movies: newmovieList  
-        // })
-        //guncel degerleri atma
-        this.setState(state =>({    /*parametre olarak var olan satate alıyoruz, var olan state gunceliyoruz  */
-            movies: newmovieList    /*olusturdugummuz newmovieList guncel halini movies'e atıyoruz */ 
+
+        this.setState(state => ({    
+            movies: newmovieList    
         }))
     }
 
     //search islemi
-    searcMovie =(event)=>{
+    searcMovie = (event) => {
         // console.log(event.target.value)
-        this.setState({searcQuery: event.target.value})
+        this.setState({ searcQuery: event.target.value })
     }
 
 
     render() {
         //filitreleme islemi
-        let filterMovies = this.state.movies.filter(    
-            (movie)=>{  /* filimler movie parametresinde  */
-                return movie.name.toLocaleLowerCase().indexOf(this.state.searcQuery.toLocaleLowerCase()) !== -1     /*aranan deger filim'in iceriside var ise geriye, gon yoksa -1 bos don */ 
+        let filterMovies = this.state.movies.filter(
+            (movie) => {  /* filimler movie parametresinde  */
+                return movie.name.toLocaleLowerCase().indexOf(this.state.searcQuery.toLocaleLowerCase()) !== -1     /*aranan deger filim'in iceriside var ise geriye, gon yoksa -1 bos don */
             }
         )
 
@@ -52,16 +89,16 @@ class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <SearchBar 
-                            propsSearchMovie ={this.searcMovie} /*searcMovie func. , propsSearchMovie degiskenin icerisine at ve props ile gonder gonder*/
+                        <SearchBar
+                            propsSearchMovie={this.searcMovie} /*searcMovie func. , propsSearchMovie degiskenin icerisine at ve props ile gonder gonder*/
                         />
                     </div>
                 </div>
 
                 <MovieList
                     movies={filterMovies}   /* props olarak gonderme, filitrelenen ogeleri, ekranda gosterilecek kartlar*/
-                        propsDeletMovie= {this.deletMovie}
-                    />
+                    propsDeletMovie={this.deletMovie}
+                />
             </div>
         )
     }
